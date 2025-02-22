88fca2648d2b5b5eed7849cc3221333167a9b3f4
praneeth188 <praneeth.sangnal2023@vitstudent.ac.in>
2025-02-22 13:53:53 +0530

    Create q2.js

---
+++ Assignment-10/Q2/q2.js
@@ -0,0 +1,8 @@
+document.getElementById("redBtn").addEventListener("click", function() {
+    document.body.style.backgroundColor = "red";
+});
+
+document.getElementById("blueBtn").addEventListener("click", function() {
+    document.body.style.backgroundColor = "blue";
+});
+
